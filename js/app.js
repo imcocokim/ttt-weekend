c92e177ce64766bd222758630f999319288dca06
imcoco <imcoco@gmail.com>
2022-06-02 15:41:07 -0400

    set up cache elements

--- js/app.js
+++ js/app.js
@@ -7,7 +7,11 @@ let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll('square')
+console.log('square')
 
+const messageEl = document.querySelector('h2')
+console.log(messageEl)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
